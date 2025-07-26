e012a656c3e341d8fab9d9fffb00bfd80d5d85d6
R2K RAHUL <123983265+Rahul-B47@users.noreply.github.com>
2025-07-27 00:19:40 +0530

    modified document_js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,11 +1,13 @@
+// pages/_document.js (or app/document.js if you're using App Router)
+
 import { Html, Head, Main, NextScript } from "next/document";
 import Script from "next/script";
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="en">
       <Head>
-        {/* YouTube Iframe API - async load after page becomes interactive */}
+        {/* Load YouTube Iframe API after page is interactive */}
         <Script
           src="https://www.youtube.com/iframe_api"
           strategy="afterInteractive"
